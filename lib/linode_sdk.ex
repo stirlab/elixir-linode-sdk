b6ea51324a5d0f9803836aff768b5cf5af5bb3b4
Chad Phillips <chad@apartmentlines.com>
2018-01-05 09:19:56 -0800

    Application.get_env -> Application.fetch_env! where appppropriate

--- lib/linode_sdk.ex
+++ lib/linode_sdk.ex
@@ -7,7 +7,7 @@ defmodule Linode do
   use Tesla
 
   plug Tesla.Middleware.BaseUrl, Application.get_env(:linode_sdk, :api_endpoint)
-  plug Tesla.Middleware.Headers, %{"Authorization" => "token " <> Application.get_env(:linode_sdk, :access_token)}
+  plug Tesla.Middleware.Headers, %{"Authorization" => "token " <> Application.fetch_env!(:linode_sdk, :access_token)}
   plug Tesla.Middleware.JSON
   if Application.get_env(:linode_sdk, :debug_http) do
     plug Tesla.Middleware.DebugLogger
